27f78175c7143bf1595ee01104ba87f7497a873d
Nwanedilobu Egbekwu <65601832+N-DiLo@users.noreply.github.com>
2021-09-05 06:46:45 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+import 'app/app.locator.dart';
+import 'app/app.router.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      navigatorKey: StackedService.navigatorKey,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
+      title: 'ZuriChat App',
+      initialRoute: Routes.homeView,
+    );
+  }
+}
